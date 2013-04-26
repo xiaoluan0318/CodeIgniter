20dad45ff9e8267f7387ccbd2b22ede4149b34e6
Keen <keen@QM-Keen.(none)>
2013-04-26 16:08:43 -0700

    Fix some minor bugs

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'cjm3138831388';
+$db['default']['database'] = 'ci_sandbox';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/controllers/news.php
+++ application/controllers/news.php
@@ -3,19 +3,15 @@
 class news extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
-		$this->load->model('news_model');
+		$this->load->model('news_model', 'News_Model');
 	}
 	
 	public function index() {
-		$data['news'] = $this->News_model->get_news();
+		$data['news'] = $this->News_Model->get_news();
 		$data['title'] = 'News archive';
-		
-		die('<pre>'.print_r($data, TRUE).'</pre>');
-		
 		$this->load->view('templates/header', $data);
 		$this->load->view('news/index', $data);
 		$this->load->view('templates/footer', $data);
-		
 	}
 	
 	public function view($slug) {
